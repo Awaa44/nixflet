368d66ecd14aff4bb6fe5a960d29bb21e7d7d6cd
Sophie <awanae1@hotmail.fr>
2026-02-17 14:50:22 +0100

    MAJ FK avec InnoDB

---
+++ migrations/Version20260217133138.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260217133138 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE season CHANGE overview overview LONGTEXT DEFAULT NULL, CHANGE poster poster VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE season ADD CONSTRAINT FK_F0E45BA9D94388BD FOREIGN KEY (serie_id) REFERENCES serie (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE season DROP FOREIGN KEY FK_F0E45BA9D94388BD');
+        $this->addSql('ALTER TABLE season CHANGE overview overview LONGTEXT DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`, CHANGE poster poster VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`');
+    }
+}
